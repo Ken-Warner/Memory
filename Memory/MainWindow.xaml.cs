28cb5d8f5a93c56b37ab9d1a53b8bcfcdece6480
Ken-Warner <ken.c.warner@gmail.com>
2021-06-05 21:08:37 -0400

    Set up the main window for the game.

--- Memory/MainWindow.xaml.cs
+++ Memory/MainWindow.xaml.cs
@@ -24,5 +24,41 @@ namespace Memory
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// Triggers when the start button is clicked. Loads a new game window and starts the game.
+        /// </summary>
+        public void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            //todo Implement
+        }
+
+        /// <summary>
+        /// Triggers when the settings button is clicked. Loads a new settings window.
+        /// </summary>
+        public void SettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            //todo Implement
+        }
+
+        /// <summary>
+        /// Ends the game and closes the window.
+        /// </summary>
+        public void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+            e.Handled = true;
+        }
+
+        /// <summary>
+        /// Serializes the UserPreferences class that will house the settings and high scores before closing the window and
+        /// ending the game
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            //todo Serialize User Preferences class to save play mode/resources/difficulty/high scores/etc
+        }
     }
 }
